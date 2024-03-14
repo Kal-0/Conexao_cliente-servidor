4989aef8ed82052926687bf4c5ef63d24aae769b
Carlos3duuu <email.provisorio2005@gmail.com>
2024-03-14 14:00:50 -0300

    JUntando em um codigo so a parte do servidor e a parte do cliente, onde sao executados paralelamente por threads

--- server.py
+++ server.py
@@ -1,39 +1,88 @@
-import socket
+import socket, threading
+
+
+
+# Definindo a porta que vai ser utilizada para a comunicação
+HOST_PORT = 12345 
+
+
 
 def server():
-    # Create a TCP/IP socket
+    # Criando um SOcket TCP/IP
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    # Bind the socket to the address and port
-    server_socket.bind(('localhost', 12345))
+    # Linkando o socket ao HOST e a porta que será utilizada
+    server_socket.bind(('localhost', HOST_PORT)) 
     
     # Listen for incoming connections
     server_socket.listen(5)
     
-    print("Server is listening...")
+    print("HOST - listening...")
     
     while True:
-        # Accept a connection
+        # Aceitando a conexão requisitada pelo cliente
         client_socket, client_address = server_socket.accept()
         
         try:
-            print(f"Connection from {client_address}")
+            print(f"HOST - connection from {client_address}")
             
-            # Receive data from the client
-            data = client_socket.recv(1024)
-            if data:
-                print(f"Received: {data.decode()}")
+            while True:
+                # Recebendo a mensagem do cliente 
+                data = client_socket.recv(1024)
                 
-                # Send a response back to the client
-                client_socket.sendall(b"Server received your message")
-                
-            else:
-                print("No more data from client")
-                break
+                if not data:
+                    break
             
+                print(f"HOST - received: {data.decode()}")
+                
+                # Mandando mensagem de confirmação do recebimento da mensagem do cliente
+                client_socket.sendall(b"PONG")
+                
+                
         finally:
-            # Close the connection
+            # Fechando a conexão do servidor
+            print("HOST - connection close")
             client_socket.close()
+            break
+    
 
+            
+            
+def client():
+    # Criando um Socket TCP/IP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Conectando o socket ao servidor
+    client_socket.connect(('localhost', HOST_PORT))
+    
+    try:
+        # Mandando a mensagem para o servidor
+        message = "PING"
+        print(f"CLIENT - sending: {message}")
+        client_socket.sendall(message.encode())
+        
+        # Recebendo a resposta do servidor
+        data = client_socket.recv(1024)
+        print(f"CLIENT - received: {data.decode()}")
+        
+        
+    finally:
+        # Fechando a conexão do cliente
+        print("CLIENT - connection close")
+        client_socket.close()
+        
+        
+        
+        
 if __name__ == "__main__":
-    server()
+    
+    # Criando 2 threads, uma para o cliente e a outra para o servidor
+    thread_server = threading.Thread(target=server)
+    thread_client = threading.Thread(target=client)
+
+    thread_server.start()
+    thread_client.start()
+    
+    thread_server.join()
+    thread_client.join()
+    
