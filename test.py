ab67da2c3736d48ac749a5e5fedb975aa5a745c6
Kal <hiratelli.0@gmail.com>
2024-04-16 22:28:32 -0300

    checksum feito

---
+++ test.py
@@ -0,0 +1,16 @@
+import pickle
+import header
+h1 = header.COOLHeader(2, 1, "ack", 10)
+
+test1 = b"c"
+exp1 = 198
+
+payload = "Hello, World!"
+payload1 = "Hello, World!"
+
+h1.set_checksum(payload)
+print(h1.checksum)
+
+payload = "Hello World!"
+
+print(header.vef_checksum(h1, payload1, h1.checksum))
